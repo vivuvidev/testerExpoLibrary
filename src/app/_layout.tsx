c069e575a69c3acb14d84a3633be9b0a69b9951e
vivuvidev <vivuvidev@gmail.com>
2025-07-14 11:13:34 +0300

    Screen detect & network track

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,12 +1,64 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { Stack } from 'expo-router'
+import { useCallback, useEffect, useState } from 'react';
+import { Text, View } from 'react-native';
+import Entypo from '@expo/vector-icons/Entypo';
+import * as SplashScreen from 'expo-splash-screen';
+import * as Font from 'expo-font';
 import "./global.css"
+import { usePreventScreenCapture } from 'expo-screen-capture';
+import { Stack } from 'expo-router';
 
-const RootLayout = () => {
+// Keep the splash screen visible while we fetch resources
+SplashScreen.preventAutoHideAsync();
+
+// Set the animation options. This is optional.
+SplashScreen.setOptions({
+  duration: 1000,
+  fade: true,
+});
+
+export default function RootLayout() {
+  const [appIsReady, setAppIsReady] = useState(false);
+
+    // Optional: prevent screen capture globally
+  usePreventScreenCapture();
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        // Pre-load fonts, make any API calls you need to do here
+        await Font.loadAsync(Entypo.font);
+        // Artificially delay for two seconds to simulate a slow loading
+        // experience. Remove this if you copy and paste the code!
+        await new Promise(resolve => setTimeout(resolve, 2000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        // Tell the application to render
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(() => {
+    if (appIsReady) {
+      // This tells the splash screen to hide immediately! If we call this after
+      // `setAppIsReady`, then we may see a blank screen while the app is
+      // loading its initial state and rendering its first pixels. So instead,
+      // we hide the splash screen once we know the root view has already
+      // performed layout.
+      SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
   return (
-    <Stack/>
-  )
+    <View className="flex-1  bg-white"
+      onLayout={onLayoutRootView}>
+     <Stack/>
+    </View>
+  );
 }
-
-export default RootLayout
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,7 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import * as Network from 'expo-network';
 export default function App() {
+  const networkState = Network.useNetworkState();
+  console.log(`Current network type: ${networkState.type}`);
   return (
    <View className="flex-1 items-center justify-center bg-white">
       <Text className="text-xl font-bold text-blue-500">
