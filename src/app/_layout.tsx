86279a9170a59f691ccbb4b7b213e912781315df
vivuvidev <vivuvidev@gmail.com>
2025-07-14 09:15:20 +0300

    Initial commit

---
+++ src/app/_layout.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { Stack } from 'expo-router'
+import "./global.css"
+
+const RootLayout = () => {
+  return (
+    <Stack/>
+  )
+}
+
+export default RootLayout
---
+++ src/app/index.tsx
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+   <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-blue-500">
+        Welcome to Nativewind!
+      </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
